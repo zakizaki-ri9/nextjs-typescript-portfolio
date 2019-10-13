aececf2f7d7086daa0877c29d5d46c7f800c578c
lanches-yazaki-yuki <yazaki.yuuki@lanches.co.jp>
2019-10-13 10:32:27 +0900

    feat: static site

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
-const withCSS = require('@zeit/next-css')
+// 静的サイトのルーティング設定
+module.exports = {
+  exportPathMap: async () => {
+    return {
+      '/': {
+        page: '/'
+      }
+    }
+  }
+}
 
+// CSS in JSの設定
+const withCSS = require('@zeit/next-css')
 module.exports = withCSS({})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,9 @@ const Page: NextPage = props => {
   return (
     <DefaultLayout>
       <div className="bg-gray-200 p-4">
-        <span className="block text-gray-700 text-center bg-gray-400 px-4 py-2">
-          1
-        </span>
-        <span className="block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-2">
-          2
-        </span>
-        <span className="block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-2">
-          3
-        </span>
+        <span className="block text-gray-700 text-center bg-gray-400 px-4 py-2">1</span>
+        <span className="block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-2">2</span>
+        <span className="block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-2">3</span>
       </div>
     </DefaultLayout>
   )
