3cae74caecddfde0cb05fa83b552663224ebab88
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-10-15 09:21:23 +0900

    feat: transition

--- src/components/navbar/header.tsx
+++ src/components/navbar/header.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
 import '../../styles/main.css'
-import cns from 'classnames'
 
 const HeaderLink: React.FC = props => (
-  <a href="#" className="block inline-block mt-0 text-teal-200 hover:text-white mr-4">
+  <a
+    href="#"
+    className="block inline-block mt-0 text-gray-900 hover:text-gray-500 mr-4 transition-all transition-300 transition-ease"
+  >
     {props.children}
   </a>
 )
 
 const Navbar: React.FC = props => {
   return (
-    <nav className="flex h-16	absolute top-0 left-0 right-0 items-center justify-between flex-wrap bg-teal-500">
-      <span className="ml-5 text-white font-semibold text-xl tracking-tight">zaki's portfolio</span>
+    <nav className="flex h-16 items-center justify-between flex-wrap bg-gray-100">
+      <span className="ml-5 text-gray-900 font-semibold text-xl tracking-tight">
+        zaki's portfolio
+      </span>
       <div className="flex">
         <HeaderLink>Podcast</HeaderLink>
         <HeaderLink>Blog</HeaderLink>
